97a5b666ce6a38521748a5a03ff016bca46f5a9e
Volraiden <Danielleebuckley@gmail.com>
2025-11-10 22:49:45 +0500

    .

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Ensure proper output for Netlify
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
-
